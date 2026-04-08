95a1e34df2cd962963c99d667fc1ddfe155f7148
Carlos Lopez <lopez.carlos.4807@eam.edu.co>
2026-04-08 11:42:14 -0500

    initial commit

---
+++ src/main/java/com/Quantum/QZMotorCenterAuth/QzMotorCenterAuthApplication.java
@@ -0,0 +1,13 @@
+package com.Quantum.QZMotorCenterAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QzMotorCenterAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QzMotorCenterAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Quantum/QZMotorCenterAuth/QzMotorCenterAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Quantum.QZMotorCenterAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QzMotorCenterAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
